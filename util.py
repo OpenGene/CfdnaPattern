a7c3fd08eb48babd4c23a2c55e40ccbd67b05d28
Shifu Chen <chen@haplox.com>
2016-11-17 09:39:41 +0800

    skip files for dataset washing

--- util.py
+++ util.py
@@ -1,10 +1,18 @@
 import sys, os
 import fastq
+import skip
+
+def is_to_skip(path):
+    for item in skip.file_to_skip:
+        if item[0] in path and item[1] in path:
+            return True
+    return False
 
 def get_arg_files():
     files = []
     for f in sys.argv:
         if fastq.is_fastq(f) and os.path.exists(f):
             path = os.path.join(os.getcwd(), f)
-            files.append(path)
+            if not is_to_skip(path):
+                files.append(path)
     return files
